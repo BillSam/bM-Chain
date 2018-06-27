a45d8a263a234eb24f9b59ffe4614b1fee14a1a7
work <sam.wangui13@gmail.com>
2018-06-27 10:30:31 +0300

    Second commit

---
+++ block.js
@@ -0,0 +1,38 @@
+const SHA256  = require('crypto-js/sha256');
+class Block{
+  constructor(timeStamp, lastHash, hash, data){
+    this.timeStamp = timeStamp;
+    this.lastHash = lastHash;
+    this.hash =hash;
+    this.data = data;
+  }
+
+  toString(){
+    return `Block -
+      TimeStamp: ${this.timeStamp}
+      LastHash : ${this.lastHash.toString(0,10)}
+      Hash     : ${this.hash.toString(0,10)}
+      Data     : ${this.data}`;
+    }
+
+
+  static genesis(){
+    return new this('Genesis time','----','f51-h54',[]);
+
+  }
+
+  static mineBlock(lastBlock,data){
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp,lastHash,data);
+
+
+    return new this(timestamp,lastHash,hash,data);
+  }
+
+  static hash(timestamp,lastHash,data){
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
+}
+
+module.exports = Block;
