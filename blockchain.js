996c518f144f06f2d9c9230900d32e4d24280764
work <sam.wangui13@gmail.com>
2018-06-28 08:28:38 +0300

    Added Chain capability

--- blockchain.js
+++ blockchain.js
@@ -14,6 +14,27 @@ class BlockChain{
 
         return block;
     }
+
+    isValidChain(chain){
+        //check if chain start with genesis block
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
+        //compare block's lasthash with previous block hash one  in the chain
+        for(let i=1; i < chain.length; i++){
+            const block = chain[i];
+            const lastBlock = chain[i-1];
+
+            if(block.lastBlock !== lastBlock.hash || 
+                block.hash !== Block.blockHash(block)){
+            return false;
+                }
+                
+        }
+        return true;
+
+    }
+
+
 }
 
 module.export = BlockChain;
